941e67393c6cdfa51aa1b14520ebb161a6bc7527
Aybike48 <90464121+Aybike48@users.noreply.github.com>
2022-01-28 17:12:14 +0300

    Add files via upload

---
+++ input_elementleri/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="container">
+        <form v-if!="göster">
+            <div class="row">
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <label for="name">İsim</label>
+                    <div class="mt-5">
+                        <input class="form-group" placeholder="İsminizi Giriniz" type="text" v-model="userData.isim" >
+                    </div>
+                    <label for="name">email</label>
+                    <div class="mt">
+                        <input class="form-group" placeholder="Email Giriniz " type="email" v-model="userData.email" >
+                    </div>
+                    <label for="name">şifre</label>
+                    <div class="mt">
+                        <input class="form-group" placeholder="Şifre Giriniz" type="password" v-model="userData.sifre" >
+
+                    </div>
+                    <h3>Cinsiyet</h3>
+                    <div class="mt">
+                        <input class="form-group" type="radio" v-model="userData.cinsiyet" value="KADIN"> KADIN
+                    </div>
+                    <div class="mt">
+                        <input class="form-group" type="radio" v-model="userData.cinsiyet" value="erkek"> erkek 
+                    </div>   
+                    <div class="mt">
+                        <input class="form-group" type="radio" v-model="userData.cinsiyet" value="yok"> Belirtmek İstemiyorum
+                    </div>                  
+                    <button class="btn btn-primary" @click!="göster" @click.prevent="submit" >Gönder</button>
+                </div>
+            </div>
+        </form>
+        <hr>
+        <div class="row" v-if="!göster">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h4>Girdiğiniz Bilgier</h4>
+                    </div>
+                    <div class="panel-body">
+                        <p>Tam Ad:{{userData.isim}}</p>
+                        <p>E-mail: {{userData.email}} </p>
+                        <p>Şifre: {{userData.sifre}}</p>
+                        <p>Cinsiyet: {{userData.cinsiyet}}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+               userData : {                  
+                    isim:" ",
+                    email:" ",
+                    sifre:" ",
+                    cinsiyet:" "               
+               },
+               göster : false               
+            };           
+        }
+    }
+</script>
+
+<style>
+</style>
